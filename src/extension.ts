1e83363f0ed7ed34b4c41660795f17b6fa2b43b7
zsytssk <zsytssk@gmail.com>
2019-06-18 07:43:11 +0800

    master:[remove] treeview

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,22 @@
 
 import * as vscode from 'vscode';
 import { listCmd, listFile } from './main';
-import { DepNodeProvider } from './treeView';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand('cmdFlow.listCmd', async () => {
-      listCmd();
-    }),
+    vscode.commands.registerCommand(
+      'cmdFlow.listCmd',
+      async () => {
+        listCmd();
+      },
+    ),
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand('cmdFlow.listFile', async () => {
-      listFile();
-    }),
-  );
-
-  const nodeDependenciesProvider = new DepNodeProvider(
-    vscode.workspace.rootPath,
-  );
-  vscode.window.registerTreeDataProvider(
-    'cmdFlowList',
-    nodeDependenciesProvider,
+    vscode.commands.registerCommand(
+      'cmdFlow.listFile',
+      async () => {
+        listFile();
+      },
+    ),
   );
 }
